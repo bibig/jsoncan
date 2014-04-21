6a585b84d292de47b9ea6624409130984492ade5
bibig <liushuxun@gmail.com>
2014-04-21 10:32:58 +0800

    remove backup feature, can be replaced by tar

--- index.js
+++ index.js
@@ -19,36 +19,5 @@ exports = module.exports = function (path, tableSchemas, validateMessages) {
       _this.table(name).refresh();
     });
   };
-  
-  
-  this.backup = function (file, callback) {
-    var async = require('async');
-    var fs = require('fs');
-    var self = this;
-    var db = {};
-    var tables = Object.keys(this.conn.tables);
-    
-    function tableBackup (name, callback) {
-      var table = self.open(name);
-      table.query().exec(function (err, records) {
-        if (err) {
-          callback(err);
-        } else {
-          db[name] = records;
-          callback();
-        }
-      });
-    }
-    
-    async.each(tables, tableBackup, function (err) {
-      if (err) { 
-        callback(err);
-      } else {
-        fs.writeFile(file, JSON.stringify(db), callback);
-      }
-    });
-  }; // end of backup
-  
-  // todo ...
-  this.recover = function (file, callback) {};
+
 };
